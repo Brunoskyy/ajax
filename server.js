51d8e513fb20ca4599c98f4a8bc2a2c54340d56c
Artur Bruno <arturbrunoferreira@gmail.com>
2019-10-15 00:19:00 -0300

    Lib multer adicionada ao projeto para tratar as requisições.

--- server.js
+++ server.js
@@ -6,5 +6,25 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send(`Você acessou em ${new Date}`))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.send('Ocorreu um erro.')
+        }
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
